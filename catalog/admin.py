ef443ad40884227b8aaa19aab91a2adbc5be89b8
Nazeeruddin <shaik.nazeeruddin@hpe.com>
2018-04-23 15:23:24 +0530

    migrations

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,30 +1,60 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Author, Genre, Book, BookInstance
 
-#admin.site.register(Book)
-#admin.site.register(Author)
+from .models import Author, Genre, Book, BookInstance, Language
 
-# Define the admin class
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
-# Register the admin class with the associated model
-admin.site.register(Author, AuthorAdmin)
+"""
+# Minimal registration of Models.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(BookInstance)
+admin.site.register(Genre)
+admin.site.register(Language)
+"""
 
 admin.site.register(Genre)
-#admin.site.register(BookInstance)
+admin.site.register(Language)
 
-# Register the Admin classes for Book using the decorator
+class BooksInline(admin.TabularInline):
+    """
+    Defines format of inline book insertion (used in AuthorAdmin)
+    """
+    model = Book
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    Administration object for Author models. 
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - orders fields in detail view (fields), grouping the date fields horizontally
+     - adds inline addition of books in author view (inlines)
+    """
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+
 class BooksInstanceInline(admin.TabularInline):
+    """
+    Defines format of inline book instance insertion (used in BookAdmin)
+    """
     model = BookInstance
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author','display_genre')
 
-# Register the Admin classes for BookInstance using the decorator
+class BookAdmin(admin.ModelAdmin):
+    """
+    Administration object for Book models. 
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - adds inline addition of book instances in book view (inlines)
+    """
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+admin.site.register(Book, BookAdmin)
+
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
--- catalog/forms.py
+++ catalog/forms.py
@@ -1,10 +1,13 @@
-from django import forms
-
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 import datetime #for checking renewal date range.
     
+from django import forms
+
 class RenewBookForm(forms.Form):
+    """
+    Form for a librarian to renew books.
+    """
     renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
 
     def clean_renewal_date(self):
@@ -13,8 +16,7 @@ class RenewBookForm(forms.Form):
         #Check date is not in past. 
         if data < datetime.date.today():
             raise ValidationError(_('Invalid date - renewal in past'))
-
-        #Check date is in range librarian allowed to change (+4 weeks).
+        #Check date is in range librarian allowed to change (+4 weeks)
         if data > datetime.date.today() + datetime.timedelta(weeks=4):
             raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
 
